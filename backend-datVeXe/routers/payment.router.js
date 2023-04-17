6a99d41f48bbaa97b66c1ff2cc693f07898c0b2e
Nguyễn Hoàng Anh <73330650+Hoanganh241@users.noreply.github.com>
2023-04-17 20:05:27 +0700

    up

--- backend-datVeXe/routers/payment.router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const {paymentController} = require("../controllers/pay.controller");
-
-const PaymentRouter = express.Router();
-
-PaymentRouter.post("/", paymentController);
-
-module.exports = {PaymentRouter};
